9ef4c053674db0d046166be3446ee692f245842a
iftekharul152811 <adnansarker792@gmai.com>
2022-11-27 14:04:18 +0600

    product add ,get,delete api added

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ async function run() {
     try {
         const categoryOptionCollention = client.db('pustokBitan').collection('categoriesCollection');
         const usersCollention = client.db('pustokBitan').collection('users');
+        const bookingCollention = client.db('pustokBitan').collection('bookings');
+        const addProductCollention = client.db('pustokBitan').collection('addProduct');
 
         // get categories from server
         app.get('/categories', async (req, res) => {
@@ -41,6 +43,54 @@ async function run() {
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollention.insertOne(user);
+            res.send(result);
+        })
+
+
+        //get current users booking data
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const booking = await bookingCollention.find(query).toArray();
+            res.send(booking);
+        })
+
+        //post booking of a book data
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollention.insertOne(booking);
+            res.send(result);
+        })
+
+        //delete a booking data
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollention.deleteOne(query);
+            res.send(result);
+        })
+
+        //post new-product data
+        app.post('/addProduct', async (req, res) => {
+            const addProduct = req.body;
+            const resultAddProduct = await addProductCollention.insertOne(addProduct);
+            res.send(resultAddProduct);
+        })
+
+        //get current users all products data
+        app.get('/addProduct', async (req, res) => {
+            // const email = req.query.email;
+            // const query = { email: email };
+            const query = {};
+            const addProduct = await addProductCollention.find(query).toArray();
+            res.send(addProduct);
+        })
+
+        //delete a product data
+        app.delete('/addProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await addProductCollention.deleteOne(query);
             res.send(result);
         })
 
