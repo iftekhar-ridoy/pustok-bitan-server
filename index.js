a2598e3a3e676ec9f3e7d54463de6d44eeb200c2
iftekharul152811 <adnansarker792@gmai.com>
2022-11-28 18:52:13 +0600

    admin role added

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// varifyJWT 
+function varifyJWT(req, res, next) {
+    console.log('token inside verify', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 
 async function run() {
     try {
@@ -76,6 +94,15 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await usersCollention.deleteOne(query);
             res.send(result);
+        })
+
+
+        // admin role 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollention.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
         })
 
 
